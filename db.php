8ec8eda8c8842a5eb17d65f6ef42cb45b37ba94e
Q3qachen <2114532260@qq.com>
2026-05-14 15:16:29 +0800

    备份一下版本

--- db.php
+++ db.php
@@ -9,8 +9,11 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ]);
+    
 } catch (PDOException $e) {
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode(['error' => '数据库连接失败']);
     exit;
 }
+
+// 备份一下版本
